ad1c3401986e1984b2bf34abd800869815ee2aa3
wata548 <162409030+wata548@users.noreply.github.com>
2024-12-21 16:45:46 +0900

    make usual vector file

---
+++ Assets/TestScirpts/BallMovement.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BallMovement : MonoBehaviour
+{
+    Rigidbody2D player = null;
+    [SerializeField] private float power = 30;
+    [SerializeField] private Vector2 moveDirection = Vector2.zero;
+    
+    private readonly Vector2[] directions = new Vector2[] {
+        Vector2.up,
+        Vector2.down,
+        Vector2.left,
+        Vector2.right
+    };
+    
+    void Awake()
+    {
+        if (player == null) {
+            player = GetComponent<Rigidbody2D>();
+            player.AddForce(power * (moveDirection.normalized));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+
+        var collisionDir = (other.transform.position - transform.position).normalized;
+
+        float best = 0;
+        Vector2 ballToBlockDirection = Vector2.zero;
+
+        foreach (var direction in directions) {
+
+            float value = direction.DotProduction(collisionDir); 
+           
+            if (best < value) {
+
+                ballToBlockDirection = direction;
+                best = value;
+            }
+        }
+
+        var playerVelocity = player.linearVelocity.normalized;
+
+        float result = playerVelocity.DotProduction(ballToBlockDirection);
+        transform
+    }
+}
